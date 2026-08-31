9709ae1fd9dcf7b3eff0fd23ed545a64345cd6d3
GEslinger <30093287+GEslinger@users.noreply.github.com>
2026-08-31 14:35:34 -0400

    Proof of concept xml parse

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,12 @@
 const std = @import("std");
+const dprint = std.debug.print;
 const Io = std.Io;
+
+const MAX_FILE_SIZE = 1_000_000;
 
 pub fn main(init: std.process.Init) !void {
     // Prints to stderr, unbuffered, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    dprint("Hello, world!\n", .{});
 
     // This is appropriate for anything that lives as long as the process.
     const arena: std.mem.Allocator = init.arena.allocator();
@@ -20,48 +23,88 @@ pub fn main(init: std.process.Init) !void {
     // Stdout is for the actual output of your application, for example if you
     // are implementing gzip, then only the compressed bytes should be sent to
     // stdout, not any debugging messages.
-    var stdout_buffer: [1024]u8 = undefined;
-    var stdout_file_writer: Io.File.Writer = .init(.stdout(), io, &stdout_buffer);
-    const stdout_writer = &stdout_file_writer.interface;
+    // var stdout_buffer: [1024]u8 = undefined;
+    // var stdout_file_writer: Io.File.Writer = .init(.stdout(), io, &stdout_buffer);
+    // const stdout_writer = &stdout_file_writer.interface;
 
-    try stdout_writer.flush(); // Don't forget to flush!
-}
+    // try stdout_writer.flush(); // Don't forget to flush!
+    var buf: [1024]u8 = undefined;
 
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
+    var file = try Io.Dir.openFile(Io.Dir.cwd(), io, args[1], .{});
+    defer file.close(io);
+    var f_reader = file.reader(io, &buf);
 
-test "fuzz example" {
-    try std.testing.fuzz({}, testOne, .{});
-}
+    const contents = try f_reader.interface.allocRemaining(arena, Io.Limit.limited(MAX_FILE_SIZE));
+    dprint("{s}\n", .{contents[0..30]});
+    f_reader.pos = 0;
+    // var line_iterator = std.mem.tokenizeAny(u8, contents, "\r\n");
 
-fn testOne(context: void, smith: *std.testing.Smith) !void {
-    _ = context;
-    // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
+    // // Print 3 lines
+    // dprint("First 3 lines:\n", .{});
+    // for (0..2) |_| {
+    //     dprint("{s}\n", .{line_iterator.next().?});
+    // }
+    // line_iterator.reset();
 
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(u8) = .empty;
-    defer list.deinit(gpa);
-    while (!smith.eos()) switch (smith.value(enum { add_data, dup_data })) {
-        .add_data => {
-            const slice = try list.addManyAsSlice(gpa, smith.value(u4));
-            smith.bytes(slice);
-        },
-        .dup_data => {
-            if (list.items.len == 0) continue;
-            if (list.items.len > std.math.maxInt(u32)) return error.SkipZigTest;
-            const len = smith.valueRangeAtMost(u32, 1, @min(32, list.items.len));
-            const off = smith.valueRangeAtMost(u32, 0, @intCast(list.items.len - len));
-            try list.appendSlice(gpa, list.items[off..][0..len]);
-            try std.testing.expectEqualSlices(
-                u8,
-                list.items[off..][0..len],
-                list.items[list.items.len - len ..],
-            );
-        },
+    // Loop setup
+    var parsing = true;
+    parsing = true;
+    var loops: u64 = 0;
+    var depth: u64 = 0;
+
+    try parse: while (parsing == true) : (loops += 1) {
+
+        // Begin to parse, looking for <
+        _ = f_reader.interface.takeDelimiterInclusive('<') catch break;
+
+        const next_char = f_reader.interface.peek(1) catch break ParseError.BadTag;
+        var tag_type: TagType = switch (next_char[0]) {
+            '?' => .ProcessInstruction,
+            '/' => .Close,
+            '!' => blk: {
+                const check_comment = f_reader.interface.peek(3) catch break :parse ParseError.BadTag;
+                if (std.mem.eql(u8, check_comment, "!--")) break :blk .Comment;
+                const check_data = f_reader.interface.peek(8) catch break :blk .Declaration;
+                if (std.mem.eql(u8, check_data, "![CDATA[")) break :blk .Data;
+                break :blk .Declaration;
+            },
+            else => .Open,
+        };
+
+        const tag: []u8 = try switch (tag_type) {
+            // TODO: Enforce proper ending of all tag types
+            else => f_reader.interface.takeDelimiterExclusive('>') catch ParseError.Unclosed,
+        };
+
+        if (tag.len < 1) break ParseError.BadTag;
+        if (tag[tag.len - 1] == '/') tag_type = .OpenClose;
+
+        if (tag_type == .Open) depth += 1;
+        if (tag_type == .Close) depth -= 1;
+
+        dprint("D: {d}, Type: {any}, Len {d}, chars: {s}\n", .{ depth, tag_type, tag.len, tag });
+
+        if (loops > 1000) {
+            dprint("TOO MANY LOOPS!\n", .{});
+            break ParseError.Overflow;
+        }
     };
+
+    if (depth != 0) return ParseError.Unclosed;
 }
+
+const TagType = enum {
+    Open,
+    Close,
+    OpenClose,
+    ProcessInstruction,
+    Comment,
+    Data,
+    Declaration,
+};
+
+const ParseError = error{
+    Overflow,
+    Unclosed,
+    BadTag,
+};
